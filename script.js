01be5298b644731dd199e69ba27bff3a16534818
KosaiToko <81806266+KosaiToko@users.noreply.github.com>
2022-12-22 13:38:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+'use strict';
+
+//№1
+let a1 = 7 + 2, 
+a2 = 7 - 3,
+a3 = 2 * 4,
+a4 = 4 / 2;
+console.log(`${a1}, ${a2}, ${a3}, ${a4}`);
+
+//№2
+let height = 23,
+width = 10,
+s = height * width;
+console.log(`${s}`);
+
+//№3
+let b1 = 5 % 3,
+b2 = 3 % 5,
+b3 = 5 + '3',
+b4 = '5' - 3,
+b5 = 75 + 'кг';
+console.log(`${b1}, ${b2}, ${b3}, ${b4}, ${b5}`);
+
+//№4
+let heightC = 10,
+dC = 4,
+v = heightC * dC;
+console.log(`${v}`);
+
+//№5
+let a = 10,
+b = 2,
+sum = a + b,
+dif = a - b,
+pro = a * b,
+div = a / b;
+console.log(`${sum}, ${dif}, ${pro}, ${div}`);
+
+//№6
+let c = 15,
+d = 2,
+result = c + d;
+console.log(`${result}`);
+
+//№7
+let z = 10,
+x = 2,
+n = 5;
+console.log(`${z+x+n}`);
+
+//№8
+let z1 = 17,
+x1 = 10,
+n1 = z1 - x1,
+h = 7,
+result1 = n1 + h;
+console.log(`${result1}`);
+
+//№9 
+let name = 'Давид';
+console.log(`${'Привет, '}` + `${name}` + `${'!'}`);
+
+//№10
+let age = '17';
+console.log(`${'Мне '}` + `${age}` + `${' лет!'}`);
+
+//№11
+let str = 'abcde';
+console.log(`${str[0]}` + `${str[2]}` + `${str[4]}`);
+
+//№12
+let sec = 1,
+min = sec * 60,
+hr = min * 60,
+day = hr * 24,
+mnth = day * 30;
+console.log(`${hr}, ${day}, ${mnth}`);
+
+//№13
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+console.log(`${num}`);
+
+//№14
+let numb = 10;
+numb++;
+numb++;
+numb--;
+console.log(`${numb}`);
